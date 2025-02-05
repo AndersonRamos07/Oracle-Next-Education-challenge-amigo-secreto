dd3015d16a62a61d1fe7b8e00ec87fc0bc33d658
Anderson Ramos <andersonramos07@live.com>
2025-02-05 16:51:35 -0300

    pequenos retoques

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ const listarAmigo = (pAmigo) =>
     let criarItem = document.createElement('li');   //cria item da lista com o nome do amigo
         criarItem.append(pAmigo);
         criarItem.onclick = selecionarSeuNome;
+        criarItem.style.cursor = 'pointer';
     listaDeAmigos.append(criarItem);
     listaDeNomes.push(pAmigo);
 };
@@ -54,9 +55,13 @@ const evidenciarNome = (pNome) =>
 {   // realca o nome selecionado
     let item = pNome;
     let nomes = document.querySelectorAll('li');
-    if(item.style.backgroundColor != 'lightgreen'){
-        nomes.forEach((nome) => nome.removeAttribute('style'));
-        item.style.backgroundColor = 'lightgreen';
+    if(item.style.backgroundColor != '#4B69FD'){
+        nomes.forEach((nome) =>
+        {   nome.removeAttribute('style');
+            nome.style.cursor = 'pointer';                
+        });
+        item.style.backgroundColor = '#4B69FD';
+        item.style.color = '#FFF';
         item.style.margin = '5px';
         item.style.padding = '5px';
         item.style.textAlign = 'center';
@@ -108,12 +113,12 @@ const criarBotao = () =>
     let botaoContainer = document.querySelector('.button-container');
     let criarBotao = document.createElement('button');  //cria um botao para refresh
         criarBotao.style.backgroundColor = 'lightblue';
-        criarBotao.append('OK, passar pro proximo...');
+        criarBotao.append('OK, passar para o próximo...');
         criarBotao.className = ['botao-OK'];
         criarBotao.onclick = validarSorteio;
     botaoContainer.append(criarBotao);
     botaoSorteio.style.visibility = 'hidden';   //oculta botao de sortear
-    botaoSorteio.style.margin = '-10%';
+    botaoSorteio.style.margin = '-12.5%';
 };
 
 const validarSorteio = () =>
